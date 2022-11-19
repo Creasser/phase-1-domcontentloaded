a810f5459bed09175e2732b85aa7ff571ac35670
Blake Creasser <blakecreasser@aggienetwork.com>
2022-11-18 21:27:53 -0600

    commit tree 880e9f64bef6309b6bc88072f1833cb4e2a4ed7d

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+// Your code goes her
+document.addEventListener('DOMContentLoaded', function(){
+    const text = document.querySelector('#text')
+    text.textContent = 'This is really cool!'
+})
